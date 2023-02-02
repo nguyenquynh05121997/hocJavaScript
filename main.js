a4fe94705ce659cbe926f2ba4d828879d9c8be53
nguyenquynh05121997 <nguyenquynh05121997@gmail.com>
2023-02-02 23:42:49 +0900

    tao form

---
+++ main.js
@@ -0,0 +1,34 @@
+// var myString = "Hoc code khong kho code code";
+
+// console.log(myString.replace(/code/g, "Code"));
+// console.log(myString.toUpperCase())
+// console.log(myString.toLowerCase())
+// console.log(myString.split(' '))
+
+// var student = {
+//     name : "nguyen van long",
+//     age : 23,
+//     address : "thanh khe thanh ha hai duong"
+// };
+
+// student.email = "nguyenlong@gmail.com"
+// console.log(student.email);
+
+// var User = function(firstName,lastName,age) 
+// {
+//     this.firstName =firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.getName = function(){
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+// var author = new User("quynh","nguyen",23);
+// var user = new User("long","nguyen",19);
+
+// author.title  = "chia se noi dung";
+// user.comment  ="mai di lam nhes";
+// // console.log(author.constructor ===User);
+// console.log(author.getName());
+// console.log(user.getName());
+
